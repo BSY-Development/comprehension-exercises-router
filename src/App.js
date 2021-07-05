b741e2cbae8180ebb87be8b250675adb2b6d3730
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-07-05 15:14:06 -0300

    Adicionado uma Route para o componente About

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
+import About from './components/About'
 import { BrowserRouter, Route } from 'react-router-dom';
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
       </BrowserRouter>
     );
   }
